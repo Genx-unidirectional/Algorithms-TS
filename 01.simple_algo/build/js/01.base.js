404f743c71c5dbcef713befda1b528ae47d4153e
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-08 23:33:23 +0530

    feat: naive search algorithm

--- 01.simple_algo/build/js/01.base.js
+++ 01.simple_algo/build/js/01.base.js
@@ -58,3 +58,19 @@ console.log(binaryNumFind([
     1, 2, 3, 393, 3303, 2020202, 11010101, 292929292, 399393939393,
     3939393939399393939, 39393939393939393939393939393939,
 ], 3939393939399393939));
+//Naive search algorithm
+// Find the substring in the string
+function findSubStr(mainStr, subStr) {
+    if (subStr.length > mainStr.length)
+        return false;
+    for (let i = 0; i < mainStr.length; i++) {
+        for (let j = 0; j < subStr.length; j++) {
+            if (mainStr[i + j] !== subStr[j])
+                break;
+            if (j === subStr.length - 1)
+                return true;
+        }
+    }
+}
+console.log(findSubStr("timeatshit", "tsh"));
+// The complexity of naive search algorithm is O(n^2)
--- 01.simple_algo/src/01.base.ts
+++ 01.simple_algo/src/01.base.ts
@@ -71,3 +71,18 @@ console.log(
     3939393939399393939
   )
 );
+
+//Naive search algorithm
+// Find the substring in the string
+function findSubStr(mainStr: string, subStr: string) {
+  if (subStr.length > mainStr.length) return false;
+  for (let i = 0; i < mainStr.length; i++) {
+    for (let j = 0; j < subStr.length; j++) {
+      if (mainStr[i + j] !== subStr[j]) break;
+      if (j === subStr.length - 1) return true;
+    }
+  }
+}
+
+console.log(findSubStr("timeatshit", "tsh"));
+// The complexity of naive search algorithm is O(n^2)
