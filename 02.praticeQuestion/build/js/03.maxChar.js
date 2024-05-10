fd3881d006ed5773a5b5120394cfdd3a15c31e7b
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-05-10 10:11:11 +0530

    feat: max character in string problem

---
+++ 02.praticeQuestion/build/js/03.maxChar.js
@@ -0,0 +1,20 @@
+"use strict";
+//Q-find maximum character appear in the string
+//In order to get the maximum appearance of thr character we can store the appearance in object
+function maxAppear(str) {
+    let charObj = {};
+    let max = 0;
+    let maxChar = "";
+    for (let char of str) {
+        charObj[char] = ++charObj[char] || 1;
+    }
+    for (let key in charObj) {
+        if (charObj[key] > max) {
+            max = charObj[key];
+            maxChar = key;
+        }
+    }
+    console.log(max);
+    return maxChar;
+}
+console.log(maxAppear("tttmmmtt"));
